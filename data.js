bbbf64a91aef84ccb7a556ae9506d903a676f4fc
lexxdaniloff <55055797+lexdanilov@users.noreply.github.com>
2026-06-29 15:37:08 +0300

    upload

---
+++ data.js
@@ -0,0 +1,395 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "Вид с высоты",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09495302238301129,
+          "pitch": 0.0360118678781749,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -0.937068660345906,
+          "pitch": 0.8453333081934016,
+          "rotation": 0,
+          "target": "3-----02"
+        },
+        {
+          "yaw": 1.5730656536447931,
+          "pitch": 1.5297728474340317,
+          "rotation": 0,
+          "target": "2-----01"
+        },
+        {
+          "yaw": -2.7029840158026843,
+          "pitch": 0.2720903569325479,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 3.076677810951585,
+          "pitch": 0.2193375709740888,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Итальянский сад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16236825604683247,
+          "pitch": -0.04883357292360202,
+          "rotation": 0,
+          "target": "2-----01"
+        },
+        {
+          "yaw": -0.3984383760894925,
+          "pitch": -0.10320983028631758,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.15679778832572033,
+          "pitch": -0.31296502215115574,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 0.0019047385097028524,
+          "pitch": -0.04758301797577147,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-----01",
+      "name": "Центральный сад - 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3337026263392353,
+          "pitch": 0.08795720346612157,
+          "rotation": 0,
+          "target": "3-----02"
+        },
+        {
+          "yaw": 0.0045718795352804875,
+          "pitch": -0.48199619520668335,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 1.3171110224921723,
+          "pitch": -0.024058524959198024,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.8949335280701494,
+          "pitch": -0.06325417572480951,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.4051245592441504,
+          "pitch": -0.028997133791824226,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-----02",
+      "name": "Центральный сад - 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17162835636110785,
+          "pitch": -0.08239125251601465,
+          "rotation": 0,
+          "target": "2-----01"
+        },
+        {
+          "yaw": -0.1679276637388849,
+          "pitch": -0.48115152158363017,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 0.5119392800125642,
+          "pitch": -0.044805208156098786,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.9234021527987011,
+          "pitch": -0.0449064630046081,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": -2.109254459470945,
+          "pitch": -0.03477625638024584,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Топиарный сад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0756255954197975,
+          "pitch": -0.35189633388035446,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.0909961680575844,
+          "pitch": -0.6008950790717833,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 1.4968145465349254,
+          "pitch": -0.10173305920072906,
+          "rotation": 0,
+          "target": "2-----01"
+        },
+        {
+          "yaw": 1.7013582289789317,
+          "pitch": -0.1040705275564342,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Водопад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.590014527943408,
+          "pitch": 0.06473110411604921,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": -1.6362441954301126,
+          "pitch": -0.005398141142283208,
+          "rotation": 0,
+          "target": "2-----01"
+        },
+        {
+          "yaw": -1.8614499166023748,
+          "pitch": -0.007340408777171348,
+          "rotation": 0,
+          "target": "3-----02"
+        },
+        {
+          "yaw": -1.4452628091738475,
+          "pitch": -0.018930396597660604,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -2.23544266382968,
+          "pitch": -0.3491912549468914,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Архангельское",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
